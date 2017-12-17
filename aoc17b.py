8becb273309f48a6e92c60073f7e980b7099fc4b
Yabk <jakov.ivkovic@gmail.com>
2017-12-17 06:44:18 +0100

    Actually not so correct

--- aoc17b.py
+++ aoc17b.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+#This is not completely correct solution.
+#You should be checking if some element gets inserted at index 0.
+#In that case your 0 gets shifted right.
+#For my input this solution worked, just because nothing got inserted at index 0.
 
 def main():
     steps = int(input())
